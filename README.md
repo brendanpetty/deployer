e17f7b0bbee4f82099cef7e10d6aede18a3f8cc3
brendanpetty <40818011+brendanpetty@users.noreply.github.com>
2024-09-02 14:38:48 +1000

    First operating version

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
 # Deployer
 A small library that automates deployment process for the app
 
-# To Do
-* Document this package :)
+# Install into Project
+composer require brendanpetty/deployer
+
+# Set Up Project
+
+Update the values of each export at the top here, then paste it into the terminal
+
+* SITEPATH **WILL BE DELETED** is the directory within ~ where the (sub)domain is served from (could be *public_html* or *sub*)
+* GITHUB Personal Access Token (from https://github.com/settings with *contents read* on the specified repo)
+
+```
+export SITEPATH=public_html
+export REPOPATH=brendanpetty/
+export REPONAME=myappname
+export GITHUBPAT=github_pat_xxxxxxxxxxxx
+
+rm -Rf ~/${SITEPATH}
+mkdir ~/${SITEPATH}_app
+cd ~/${SITEPATH}_app
+git clone https://${GITHUBPAT}@‌github.com/${REPOPATH}${REPONAME}.git
+ln -s ~/${SITEPATH}_app/${REPONAME}/public ~/${SITEPATH}
+echo "SITEPATH=${SITEPATH}" > ~/${SITEPATH}_app/config.sh
+echo "REPONAME=${REPONAME}" >> ~/${SITEPATH}_app/config.sh
+cd ~/${SITEPATH}_app/${REPONAME}
+composer install --no-dev
+cp .env.example .env
+sed -i '/^APP_ENV=/c\APP_ENV=production' .env
+sed -i '/^APP_DEBUG=/c\APP_DEBUG=false' .env
+php artisan key:generate
+chmod u+x vendor/brendanpetty/deployer/run.sh
+ln -s ~/${SITEPATH}_app/${REPONAME}/vendor/brendanpetty/deployer/run.sh ~/deploy_${SITEPATH}.sh
+echo "In future, call:  ~/deploy_${SITEPATH}.sh"
+```
+
+* Then edit .env file with (at least) credentials for Database & Email.
+
+# Deploy Project
+
+From terminal, run (replace * with your relevant path)
+```
+    ~/deploy_*.sh
+```
+
+Generally option 2 will do all the necessary updates work, then option 1 to exit the script.
--- run.sh
+++ run.sh
@@ -1,6 +1,134 @@
 #!/bin/bash
 
-whoami
-pwd
-git --version
-git status
+echo "***************************"
+echo "*   BEGIN DEPLOY SCRIPT   *"
+echo "***************************"
+echo ""
+
+# Determine the actual script location, resolving symlinks
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+# Set SITEPATH based on the script's directory
+SITEPATHAPP=$(echo "$SCRIPT_DIR" | awk -F'/' '{print $4}')
+
+# Set the config file path
+CONFIG_FILE=~/${SITEPATHAPP}/config.sh
+
+# Source the config file if it exists
+if [ -f "$CONFIG_FILE" ]; then
+    cat $CONFIG_FILE
+    source "$CONFIG_FILE"
+else
+    # Prompt for configuration values if the file doesn't exist
+    read -p "Enter SITEPATH (doc root location within ~ like public_html or sub): " SITEPATH
+    read -p "Enter REPONAME (not including vendor name): " REPONAME
+
+    # Save the configuration
+    echo "SITEPATH=${SITEPATH}" > $CONFIG_FILE
+    echo "REPONAME=${REPONAME}" >> $CONFIG_FILE
+fi
+
+cd ~/${SITEPATH}_app/${REPONAME}
+
+# Define the function to run the appropriate npm command
+run_npm_script() {
+    local PACKAGE_JSON="package.json"
+    # Check if the package.json file exists
+    if [[ ! -f "$PACKAGE_JSON" ]]; then
+        return
+    fi
+    # Check for 'build' script in package.json
+    if grep -q '"build":' "$PACKAGE_JSON"; then
+        npm run build
+    # Check for 'prod' script in package.json
+    elif grep -q '"prod":' "$PACKAGE_JSON"; then
+        npm run prod
+    else
+        echo "No valid 'build' or 'prod' script found in package.json."
+    fi
+}
+
+options=("Exit" "FULL UPDATE" "Git Status" "Git Fetch" "Git Pull" "Git Diff" "Git Clean (force)" "Git Reset" "Composer Install" "NPM Install" "NPM Build" "Artisan Migrate" "Artisan Cache" "Artisan Seed")
+
+while true
+do
+    echo ""
+    echo ""
+    echo "Select an action (1-${#options[@]}): "
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Exit")
+                echo "See ya!"
+                exit 0
+                ;;
+            "FULL UPDATE")
+                git pull
+                composer install --no-dev
+                npm install
+                run_npm_script
+                php artisan migrate
+                php artisan config:cache
+                php artisan route:cache
+                php artisan view:cache
+                break
+                ;;
+            "Git Status")
+                git status
+                break
+                ;;
+            "Git Fetch")
+                git fetch
+                break
+                ;;
+            "Git Pull")
+                git pull
+                break
+                ;;
+            "Git Clean (force)")
+                git clean -df
+                break
+                ;;
+            "Git Reset")
+                git reset --hard
+                break
+                ;;
+            "Git Diff")
+                git diff -R
+                git diff --cached --stat
+                git diff origin/main
+                break
+                ;;
+            "Composer Install")
+                composer install --no-dev
+                break
+                ;;
+            "NPM Install")
+                npm install
+                break
+                ;;
+            "NPM Build")
+                run_npm_script
+                break
+                ;;
+            "Artisan Migrate")
+                php artisan migrate
+                break
+                ;;
+            "Artisan Cache")
+                php artisan config:cache
+                php artisan route:cache
+                php artisan view:cache
+                break
+                ;;
+            "Artisan Seed")
+                php artisan db:seed
+                break
+                ;;
+            *)
+                echo "Invalid option $opt. Please try again"
+                ;;
+        esac
+    done
+done
